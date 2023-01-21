706e19c2e663492ca40ec5de933c0e817b73826c
Simone Civetta <viteinfinite@gmail.com>
2023-01-21 22:23:33 +0100

    First commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Hero from './Hero.svelte';
+	import Info from './Info.svelte';
+	import Footer from './Footer.svelte';
+</script>
+
+<Hero />
+<Info />
+<Footer />
---
+++ tailwind.config.cjs
@@ -0,0 +1,27 @@
+const config = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+
+	theme: {
+		colors: {
+			transparent: 'transparent',
+			current: 'currentColor',
+			'deep-space-sparkle': '#495e63ff',
+			'steel-blue': '#5985A8ff',
+			iceberg: '#7ba4c8ff',
+			flame: '#D15E37ff',
+			'gold-crayola': '#E1BE87ff',
+			lava: '#C72624ff'
+		},
+		extend: {
+			fontFamily: {
+				kaushan: ['Kaushan Script', 'cursive'],
+				berkshire: ['Berkshire Swash', 'sans-serif'],
+				sans: ['Signika Negative', 'sans-serif']
+			}
+		}
+	},
+
+	plugins: []
+};
+
+module.exports = config;
